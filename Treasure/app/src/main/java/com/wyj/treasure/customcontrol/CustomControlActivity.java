92548a74390bb0999392ba44a3be4123c1ecf0a0
pilipala <wangyujiew@sina.com>
2017-12-25 00:56:29 +0800

    AnimatorSet1

--- Treasure/app/src/main/java/com/wyj/treasure/customcontrol/CustomControlActivity.java
+++ Treasure/app/src/main/java/com/wyj/treasure/customcontrol/CustomControlActivity.java
@@ -33,7 +33,7 @@ public class CustomControlActivity extends BaseActivity {
 
     }
 
-    @OnClick({R.id.btn_anima, R.id.btn_valueanimator,R.id.btn_objectanimator})
+    @OnClick({R.id.btn_anima, R.id.btn_valueanimator,R.id.btn_objectanimator,R.id.btn_animatorset})
     public void onViewClicked(View view) {
         Intent intent = new Intent();
         switch (view.getId()) {
@@ -45,6 +45,9 @@ public class CustomControlActivity extends BaseActivity {
                 break;
             case R.id.btn_objectanimator:
                 intent.setClass(CustomControlActivity.this, ObjectanimatorActivity.class);
+                break;
+            case R.id.btn_animatorset:
+                intent.setClass(CustomControlActivity.this, AnimatorSetActivity.class);
                 break;
         }
         startActivity(intent);
