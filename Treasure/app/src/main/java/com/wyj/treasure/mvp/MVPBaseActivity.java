30cfbe44092a44cf2a203bac40b266572c13b043
apk_wangyujie <809688547@qq.com>
2018-03-12 10:33:10 +0800

    解决冲突

--- Treasure/app/src/main/java/com/wyj/treasure/mvp/MVPBaseActivity.java
+++ Treasure/app/src/main/java/com/wyj/treasure/mvp/MVPBaseActivity.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-<<<<<<< HEAD
 import com.wyj.treasure.mvp.presenter.BasePresenter;
 import com.wyj.treasure.mvp.view.IView;
-=======
-import com.wyj.treasure.mvp.presenter.GetPhoneInfoPresenter;
->>>>>>> 217013f2994725881ac553c0bd8558de83922090
+
 
 /**
  * Created by wangyujie
@@ -17,7 +14,7 @@ import com.wyj.treasure.mvp.presenter.GetPhoneInfoPresenter;
  * TODO
  */
 
-<<<<<<< HEAD
+
 public abstract class MVPBaseActivity<P extends BasePresenter> extends AppCompatActivity implements IView {
 
     protected P mPresenter;
@@ -26,12 +23,13 @@ public abstract class MVPBaseActivity<P extends BasePresenter> extends AppCompat
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getView();
-
         mPresenter = createPresenter();
         initCommonData();
 
 
     }
+
+    public abstract void getView();
 
     /**
      * 创建Presenter
@@ -42,62 +40,17 @@ public abstract class MVPBaseActivity<P extends BasePresenter> extends AppCompat
         /*内存泄漏*/
         /*关联view*/
         if (mPresenter != null) {
-=======
-public abstract class MVPBaseActivity extends AppCompatActivity implements GetPhoneInfoContract.view {
-
-    private GetPhoneInfoContract.Presenter mPresenter;
-
-    @Override
-    public void setTime(String time) {
-
-    }
-
-    @Override
-    public void showLoading() {
->>>>>>> 217013f2994725881ac553c0bd8558de83922090
-
             mPresenter.attachView(this);
         }
     }
 
     @Override
-    public void hideLoading() {
-
-    }
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        new GetPhoneInfoPresenter(this);
-
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-        mPresenter.start();
-    }
-    @Override
     protected void onDestroy() {
         super.onDestroy();
-        /*解除关联*/
-<<<<<<< HEAD
         if (mPresenter != null) {
             mPresenter.detachView();
         }
     }
 
 
-    public abstract void getView();
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-=======
-    }
-
-    @Override
-    public void setPresenter(GetPhoneInfoContract.Presenter presenter) {
-        mPresenter = presenter;
->>>>>>> 217013f2994725881ac553c0bd8558de83922090
-    }
 }
